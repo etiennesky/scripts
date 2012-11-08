9697d4f5fdfc49b033d640b6f94c2bc6ce8a84c4
Etienne Tourigny <etourigny.dev@gmail.com>
2012-11-08 14:18:00 -0200

    mcd12q1: create netcdf file

--- process-mcd12q1.sh
+++ process-mcd12q1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #set -x
-years="2002"
+#years="2002"
+years="2009"
 
 #idir="/data/research/data/modis/mcd12q2"
 ##indexes_hv="h13v09 h13v10 h13v11 h12v09 h12v10 h12v11"
@@ -22,14 +23,15 @@ wgs_res=0.00416666666666667
 wgs_projwin="-projwin -81.5 13 -34.5 -56.0 "
 #mosaic_levs="2 4 8 16 32 64 120 128 256"
 #mosaic_outsize="-outsize 96 138"
-#vars=( "IGBP" )
-#ivars=( "MOD12Q1:Land_Cover_Type_1" )
+vars=( "IGBP" )
+ivars=( "MOD12Q1:Land_Cover_Type_1" )
 #vars=( "IGBP" "UMD" )
 #ivars=( "MOD12Q1:Land_Cover_Type_1" "MOD12Q1:Land_Cover_Type_2" )
 #vars=( "IGBP" "UMD" "LAI-fPAR" "NPP-BGC" "PFT" "QC" )
 #ivars=( "MOD12Q1:Land_Cover_Type_1" "MOD12Q1:Land_Cover_Type_2" "MOD12Q1:Land_Cover_Type_3" "MOD12Q1:Land_Cover_Type_4" "MOD12Q1:Land_Cover_Type_5" "MOD12Q1:Land_Cover_Type_QC")
-vars=""
-ivars=""
+#vars=""
+#ivars=""
+
 #Land Cover Type 1 (IGBP)*	 Class #	 8-bit unsigned	 255	 0–254
 #Land Cover Type 2 (UMD)*	 Class #	 8-bit unsigned	 255	 0–254
 #Land Cover Type 3 (LAI/fPAR)*	 Class #	 8-bit unsigned	 255	 0–254
@@ -46,12 +48,12 @@ ivars=""
 #Land Cover Property 1	 Not populated	 8-bit unsigned	 255	 0–254
 #Land Cover Property 2	 Not populated	 8-bit unsigned	 255	 0–254
 #Land Cover Property 3	 Not populated	 8-bit unsigned	 255	 0–254
-
+3
 for year in $years ; do
     echo "===================================================="
     echo $year
-    echo "#vars: "${vars[@]}
-    if [[ "$vars" -ne "" ]]; then
+    echo "vars: "${vars[@]} ${#vars[@]}
+    #if [[ "$vars" -ne "" ]]; then
     for (( i=0; i<${#vars[@]}; i++ )); do
 	var=${vars[$i]}
 	ivar=${ivars[$i]}
@@ -109,14 +111,15 @@ for year in $years ; do
         rm -f $product.$var.h??v??.$year.tif*
 
     done #vars
-    fi
+    #fi
 
     #make ibis file from IGBP file
     var=INLAND
-    nodata=100  
+    #nodata=254  
     water_id=16
     ofile=MCD12Q1.INLAND.SA.$year.msin.tif
     ofile_wgs=MCD12Q1.INLAND.SA.$year.wgs84.tif
+    ofile_nc=MCD12Q1.INLAND.SA.$year.wgs84.nc
 	ofile_wgs_mode=MCD12Q1.INLAND.SA.$year.wgs84.dominant_05.tif
 	ofile_wgs_mosaic=MCD12Q1.INLAND.SA.$year.wgs84.mosaic.tif
 	ofile_wgs_mosaic2=MCD12Q1.INLAND.SA.$year.wgs84.mosaic_05.tif
@@ -128,7 +131,8 @@ for year in $years ; do
 #        gdaladdo -ro -clean -r mode --config COMPRESS_OVERVIEW DEFLATE  $ofile 2 4 8 16 32 64 128 256 &
 #    fi
     if [ ! -f  $ofile_wgs ]; then 
-        val_repl_csv.py -csv_file /data/docs/research/project/data/igbp-inland.csv -in_id IGBP_ID -out_id INLAND_ID  $options_gtiff -a_nodata $nodata  MCD12Q1.IGBP.SA.$year.wgs84.tif $ofile_wgs
+#        val_repl_csv.py -csv_file /data/docs/research/project/data/csv/igbp-inland.csv -in_id IGBP_ID -out_id INLAND_ID  $options_gtiff -a_nodata $nodata  MCD12Q1.IGBP.SA.$year.wgs84.tif $ofile_wgs
+        val_repl_csv.py -csv_file /data/docs/research/project/data/csv/igbp-inland.csv -in_id IGBP_ID -out_id INLAND_ID  $options_gtiff MCD12Q1.IGBP.SA.$year.wgs84.tif $ofile_wgs
         gdaladdo -ro -clean -r mode --config COMPRESS_OVERVIEW DEFLATE  $ofile_wgs 2 4 8 16 32 64 128 256 &
     fi
 
@@ -150,6 +154,11 @@ for year in $years ; do
      fi
     wait
 
+    rm -f tmp?.nc
+    gdal_translate -co WRITE_BOTTOMUP=FALSE -of netcdf $ofile_wgs tmp1.nc
+    nice -n 10 cdo -f nc -b I8 setname,vegtype tmp1.nc $ofile_nc
+    rm -f tmp?.nc
+    
 done
 
 
